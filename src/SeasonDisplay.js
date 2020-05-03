eb0f0e2326778b36ad754b6304880fb2c678bb33
tyrion404 <chintan.170410116089@gmail.com>
2020-05-03 11:44:22 +0530

    initial Commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SeasonDisplay extends Component {
+    render() {
+        return (
+            <h3>Season!</h3>
+        );
+    }
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return (
+            <h3>Hello World!</h3>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
